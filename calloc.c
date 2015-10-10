80028fc5e280b7b62ce395dffe66f85369471ef2
hhmhywzr <eazow@163.com>
2015-10-10 19:28:08 +0800

    update calloc

--- calloc.c
+++ calloc.c
@@ -4,10 +4,16 @@
 int main()
 {
 	int *p = calloc(1, sizeof(int));
+    *p = 100;
+
+	int **pi = calloc(1, sizeof(int *));
+	*pi = p;
 
 	printf("%d", *p);
+	printf("%d", **pi);
 
 	free(p);
+	free(pi);
 
 	return 0;
 
