3cb802ab02ae1e6c5f22fa44ec7cabbc662a9f87
hhmhywzr <eazow@163.com>
2015-10-12 19:45:23 +0800

    update radix sort

--- radixsort.c
+++ radixsort.c
@@ -14,25 +14,51 @@ void radixsort(int *arr, int n)
             max = arr[i];
         }
     }
+
+    while(max / exp > 0) {
+
+        int bucket[BASE] = {0};
+
+        for(i=0; i<n; i++) {
+            bucket[(arr[i]/exp)%BASE]++;
+        }
+
+        for(i=1; i<BASE; i++) {
+            bucket[i] +=bucket[i-1];
+        }
+/**
+        for(i=0; i<n; i++) {
+            temp_arr[--bucket[(arr[i]/exp)%BASE]] = arr[i];
+        }
+*/
+        for(i=n-1; i>=0; i--) {
+            temp_arr[--bucket[(arr[i]/exp)%BASE]] = arr[i];
+        }
+
+        for(i=0; i<n; i++) {
+            arr[i] = temp_arr[i];
+        }
+
+        exp *= BASE;
+    }
 }
 
 int main()
 {
-    int arr[MAX] ;
+    int arr[MAX];
 
     int i, n;
 
     for(i = 0; i < MAX; i++) {
         arr[i] = rand();
-        print("%d\t", arr[i]);
     }
+    
+    radixsort(&arr[0], MAX);
 
-    radixsort(arr, MAX);
-
-    puts();
     for(i = 0; i < MAX; i++) {
-        printf("%d\t", arr[i]);
+        printf("%d\n", arr[i]);
     }
+    printf("\n");
 
     return 0;
 }
