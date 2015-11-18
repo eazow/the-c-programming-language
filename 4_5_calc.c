fcb0e5a4c07dcab3f93ab4e3b388e8098bb28eda
hhmhywzr <eazow@163.com>
2015-11-18 21:05:39 +0800

    update 4_5_calc

--- 4_5_calc.c
+++ 4_5_calc.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "4_5_calc.h"
 #define MAXOP 100
-#define NUMBER '0'
-
-int getop(char []);
-void push(double);
-double pop();
-int getch();
-void ungetch(int c);
-
-
 
 int main()
 {
@@ -49,64 +40,4 @@ int main()
 		}
 	}
 	return 0;
-}
-
-#define MAXVAL 100
-int sp = 0;
-double val[MAXVAL];
-
-void push(double f)
-{
-	if(sp < MAXVAL)
-		val[sp++] = f;
-	else
-		printf("error: stack full, can't push %g\n", f);
-}
-
-double pop()
-{
-	if(sp > 0)
-		return val[--sp];
-	else {
-		printf("error: stack empty\n");
-		return 0;
-	}
-}
-
-int getop(char s[])
-{
-	int i, c;
-
-	while((s[0] = c = getch()) == ' ' || c == '\t');
-
-	s[1] = '\0';
-	if(!isdigit(c) && c != '.')
-		return c;
-	i = 0;
-	if(isdigit(c))
-		while(isdigit(s[++i] = c = getch()));
-	if(c == '.')
-		while(isdigit(s[++i] = c = getchar()));
-	s[i] = '\0';
-	if(c != EOF) 
-		ungetch(c);
-	return NUMBER;
-}
-
-#define BUFSIZE 100
-
-char buf[BUFSIZE];
-int bufp = 0;
-
-int getch()
-{
-	return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c)
-{
-	if(bufp >= BUFSIZE)
-		printf("ungetch: too many characters\n");
-	else
-		buf[bufp++] = c;
 }
--- 4_5_stack.c
+++ 4_5_stack.c
@@ -1,55 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#define MAXOP 100
-#define NUMBER '0'
-
-int getop(char []);
-void push(double);
-double pop();
-int getch();
-void ungetch(int c);
-
-
-
-int main()
-{
-	int type;
-	double op2;
-	char s[MAXOP];
-
-	while((type = getop(s)) != EOF) {
-		switch(type) {
-			case NUMBER:
-				push(atof(s));
-				break;
-			case '+':
-				push(pop() + pop());
-				break;
-			case '*':
-				push(pop() * pop());
-				break;
-			case '-':
-				op2 = pop();
-				push(pop() - op2);
-				break;
-			case '/':
-				op2 = pop();
-				if(op2 != 0.0)
-					push(pop() / op2);
-				else
-					printf("error: zero divisor\n");
-				break;
-			case '\n':
-				printf("\t%.8g\n", pop());
-				break;
-			default:
-				printf("error: unknown command %s\n", s);
-				break;
-		}
-	}
-	return 0;
-}
+#include "4_5_calc.h"
 
 #define MAXVAL 100
 int sp = 0;
@@ -71,42 +21,4 @@ double pop()
 		printf("error: stack empty\n");
 		return 0;
 	}
-}
-
-int getop(char s[])
-{
-	int i, c;
-
-	while((s[0] = c = getch()) == ' ' || c == '\t');
-
-	s[1] = '\0';
-	if(!isdigit(c) && c != '.')
-		return c;
-	i = 0;
-	if(isdigit(c))
-		while(isdigit(s[++i] = c = getch()));
-	if(c == '.')
-		while(isdigit(s[++i] = c = getchar()));
-	s[i] = '\0';
-	if(c != EOF) 
-		ungetch(c);
-	return NUMBER;
-}
-
-#define BUFSIZE 100
-
-char buf[BUFSIZE];
-int bufp = 0;
-
-int getch()
-{
-	return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c)
-{
-	if(bufp >= BUFSIZE)
-		printf("ungetch: too many characters\n");
-	else
-		buf[bufp++] = c;
 }
